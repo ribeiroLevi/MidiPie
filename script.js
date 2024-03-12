f966c500a0c44450f93bb4522d68934cf9bc8a3a
Levi Ribeiro <leviribeiro72@gmail.com>
2024-03-12 11:10:36 -0300

    :sparkles: feat: knob inputs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let ctx;
 
 const StartButton = document.querySelector('button');
 const oscilattors = {};
+let gainKnob, freqKnob, waveKnob, delayKnob, attackKnob;
 
 StartButton.addEventListener('click', () => {
   ctx = new AudioContext();
@@ -42,27 +43,108 @@ function getMIDIMessage(message) {
     case 128: // noteOff
       noteOff(note);
       break;
-    // we could easily expand this switch statement to cover other types of commands such as controllers or sysex
+    case 176: // control change
+      handleMIDIMessage(message);
+      break;
+    // we could easily expand this switch statement to cover other types of commands such as sysex
+  }
+}
+
+function handleMIDIMessage(message) {
+  const [status, data1, data2] = message.data;
+  // Check if it's a control change message (status 0xB0)
+  if (status === 176) {
+    // Check if it's the knob you're interested in (CC number 71)
+    if (data1 === 70) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value A:', data2);
+      gainKnob = data2;
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 71) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value B:', data2);
+      freqKnob = data2;
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 72) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value C:', data2);
+      console.log(waveKnob);
+      if (data2 < 40) {
+        waveKnob = 'sine';
+      } else if (data2 >= 41 && data2 <= 90) {
+        waveKnob = 'square';
+      } else if (data2 > 90) {
+        waveKnob = 'triangle';
+      }
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 73) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value D:', data2);
+      delayKnob = parseFloat(data2);
+      if (!Number.isFinite(delayKnob)) {
+        delayKnob = 0.5; // Assign a default value if the incoming value is invalid
+      }
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 74) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value E:', data2);
+      attackKnob = parseFloat(data2 / 100);
+      if (!Number.isFinite(attackKnob)) {
+        attackKnob = 0.1; // Assign a default value if the incoming value is invalid
+      }
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 75) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value F:', data2);
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 76) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value G:', data2);
+      // You can update UI, adjust parameters, etc. here
+    }
+    if (data1 === 77) {
+      // React to knob input (data2 contains the value)
+      console.log('Knob value H:', data2);
+      // You can update UI, adjust parameters, etc. here
+    }
   }
 }
 
 function noteOn(note, vel) {
   const osc = ctx.createOscillator();
-  const oscGain = ctx.createGain();
+  const oscGain = ctx.createGain(); // Adjust this value to control attack time (in seconds)
 
-  oscGain.gain.value = 0.5;
+  oscGain.gain.value = 0; // Set initial gain to 0
+
+  const delayNode = ctx.createDelay(2); // 2 seconds of max delay
+  delayNode.delayTime.value = delayKnob; // Set delay time to 0.5 second
 
   const velocityGainAmnt = (1 / 127) * vel;
   const velocityGain = ctx.createGain();
   velocityGain.gain.value = velocityGainAmnt;
 
-  osc.type = 'triangle';
-  osc.frequency.value = midiToFreq(note);
+  osc.type = waveKnob;
+  osc.frequency.value = (midiToFreq(note) * freqKnob) / 100;
 
   osc.gain = oscGain;
   osc.connect(oscGain);
-  oscGain.connect(velocityGain);
-  velocityGain.connect(ctx.destination);
+  oscGain.connect(delayNode); // Connect oscillator to delay node
+  delayNode.connect(velocityGain); // Connect delay node to velocity gain
+  velocityGain.connect(ctx.destination); // Connect velocity gain to destination
+
+  // Apply attack to gain
+  const now = ctx.currentTime;
+  oscGain.gain.setValueAtTime(0, now); // Start from 0
+  oscGain.gain.linearRampToValueAtTime(
+    (0.5 * gainKnob) / 100,
+    now + attackKnob
+  ); // Attack to desired gain value
 
   osc.start();
   console.log(osc);
