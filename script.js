3a59f55da23d672072b0bdeb85c5d96caca714b3
RibeiroLevi <leviribeiro72@gmail.com>
2024-08-12 21:04:56 -0300

    :bug: hot fix debounce

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx;
 
-const StartButton = document.querySelector('button');
+const StartButton = document.querySelector('#ctx');
 const oscillators = {};
-let gainKnob = 50, // Initialize with a default value
-  freqKnob = 50, // Initialize with a default value
-  waveKnob = 'sine', // Initialize with a default waveform
-  delayKnob = 0.0, // Initialize with a default delay
-  attackKnob = 0.1; // Initialize with a default attack
-let note = null;
+let gainKnob = 50,
+  freqKnob = 50,
+  waveKnob = 'sine',
+  delayKnob = 0.0,
+  attackKnob = 0.1,
+  sustainKnob = 0.1;
 
 StartButton.addEventListener('click', () => {
+  let modal = document.querySelector('#modal');
   ctx = new AudioContext();
   console.log(ctx);
+  modal.style.visibility = 'hidden';
 });
 
 function midiToFreq(number) {
@@ -24,16 +26,15 @@ navigator.requestMIDIAccess().then(onMidiSuccess, onMidiFailure);
 
 function onMidiSuccess(midiAccess) {
   console.log(midiAccess);
-
   for (let input of midiAccess.inputs.values()) {
     input.onmidimessage = getMIDIMessage;
   }
 }
 
 function getMIDIMessage(message) {
-  let command = message.data[0] & 0xf0; // Ignore channel
-  let channel = message.data[0] & 0x0f; // Get channel
-  note = message.data[1];
+  let command = message.data[0] & 0xf0;
+  let channel = message.data[0] & 0x0f;
+  let note = message.data[1];
   let velocity = message.data.length > 2 ? message.data[2] : 0;
 
   console.log(
@@ -41,17 +42,17 @@ function getMIDIMessage(message) {
   );
 
   switch (command) {
-    case 144: // Note On
+    case 144:
       if (velocity > 0) {
         noteOn(note, velocity);
       } else {
         noteOff(note);
       }
       break;
-    case 128: // Note Off
+    case 128:
       noteOff(note);
       break;
-    case 176: // Control Change
+    case 176:
       handleMIDIControlChange(message);
       break;
     default:
@@ -66,17 +67,20 @@ function handleMIDIControlChange(message) {
   let wave = document.getElementById('wave');
   let delayProgress = document.getElementById('delayProgress');
   let freqProgress = document.getElementById('freqProgress');
+  let pie = document.getElementById('breathing');
 
   switch (data1) {
     case 9:
       console.log('Knob value A:', data2);
       gainKnob = data2;
       gainProgress.value = data2;
+      pie.style.width = `${data2 * 4}px`;
       break;
     case 10:
       console.log('Knob value B:', data2);
       freqKnob = data2;
       freqProgress.value = data2;
+      pie.style.filter = `saturate(${data2 + 50}%)`;
       break;
     case 11:
       console.log('Knob value C:', data2);
@@ -93,42 +97,49 @@ function handleMIDIControlChange(message) {
       break;
     case 12:
       console.log('Knob value D:', data2);
-      delayKnob = parseFloat(data2) / 100; // Assuming data2 ranges from 0 to 100
+      delayKnob = parseFloat(data2) / 100;
       if (!Number.isFinite(delayKnob)) {
-        delayKnob = 0.5; // Fallback value
+        delayKnob = 0.1;
       }
       delayProgress.value = data2;
       break;
-    case 7:
-      console.log('Knob value E:', data2);
-      attackKnob = parseFloat(data2 / 100);
-      if (!Number.isFinite(attackKnob)) {
-        attackKnob = 0.1;
-      }
-      break;
-    default:
-      console.log(`Unhandled Control Change: ${data1}, Value: ${data2}`);
-      break;
+    // case 7:
+    //   console.log('Knob value E:', data2);
+    //   attackKnob = parseFloat(data2 / 100);
+    //   if (!Number.isFinite(attackKnob)) {
+    //     attackKnob = 0.1;
+    //   }
+    //   break;
   }
 }
 
+function debounce(func, timeout = 50) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
 function noteOn(note, vel) {
+  if (oscillators[note]) {
+    noteOff(note);
+  }
+
   const osc = ctx.createOscillator();
   const oscGain = ctx.createGain();
-  const velocityGainAmnt = (1 / 127) * vel;
   const velocityGain = ctx.createGain();
 
-  // Set up nodes
   osc.type = waveKnob;
   osc.frequency.value = (midiToFreq(note) * freqKnob) / 100;
   oscGain.gain.value = 0;
 
-  // Connect nodes
   osc.connect(oscGain);
   oscGain.connect(velocityGain);
   velocityGain.connect(ctx.destination);
 
-  // Apply gain envelope
   const now = ctx.currentTime;
   oscGain.gain.setValueAtTime(0, now);
   oscGain.gain.linearRampToValueAtTime(
@@ -136,13 +147,10 @@ function noteOn(note, vel) {
     now + attackKnob
   );
 
-  // Start oscillator
   osc.start();
 
-  // Store oscillator
-  oscillators[note.toString()] = { osc, oscGain, startTime: now };
+  oscillators[note] = { osc, oscGain, startTime: now };
 
-  // Change key color
   const keyElement = document.querySelector(`.key[data-note="${note}"]`);
   if (keyElement) {
     keyElement.style.backgroundColor = '#67BFF0'; // Change to desired color
@@ -150,27 +158,26 @@ function noteOn(note, vel) {
 }
 
 function noteOff(note) {
-  const oscData = oscillators[note.toString()];
-  if (!oscData) return; // Check if the oscillator exists
+  const oscData = oscillators[note];
+  if (!oscData) return;
 
   const { osc, oscGain } = oscData;
   const now = ctx.currentTime;
 
-  // Fade out gain
-  oscGain.gain.setValueAtTime(oscGain.gain.value, now);
-  oscGain.gain.linearRampToValueAtTime(0, now + delayKnob); // Fade out over delayKnob time
+  const fadeOutTime = sustainKnob * delayKnob;
 
-  // Stop oscillator after delayKnob time
+  oscGain.gain.setValueAtTime(oscGain.gain.value, now);
+  oscGain.gain.linearRampToValueAtTime(0, now + fadeOutTime);
+
   setTimeout(() => {
     osc.stop();
     osc.disconnect();
-    delete oscillators[note.toString()];
-  }, delayKnob * 1000); // Convert delayKnob to milliseconds
+    delete oscillators[note];
+  }, fadeOutTime * 1000);
 
-  // Revert key color
   const keyElement = document.querySelector(`.key[data-note="${note}"]`);
   if (keyElement) {
-    keyElement.style.backgroundColor = ''; // Revert to original color
+    keyElement.style.backgroundColor = '';
   }
 }
 
@@ -184,7 +191,7 @@ const whiteKeys = [1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22, 24, 25];
 const blackKeys = [2, 4, 7, 9, 11, 14, 16, 19, 21, 23];
 
 window.onload = () => {
-  let midiNote = 60; // Starting from C4
+  let midiNote = 60;
   for (let index = 1; index <= 25; index++) {
     let div = document.createElement('div');
     if (whiteKeys.includes(index)) {
